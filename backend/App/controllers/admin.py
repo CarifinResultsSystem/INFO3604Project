76337bfe8289ddd702093c95f51431dc599063b9
Jameel Ali <J1m2e3A4i@gmail.com>
2026-02-19 07:24:44 -0400

    Update admin.py

--- backend/App/controllers/admin.py
+++ backend/App/controllers/admin.py
@@ -1,4 +1,4 @@
-from backend.App.models import User
+from backend.App.models import User, Event
 from backend.App.database import db
 
 def assignRole(userID, role):
@@ -6,4 +6,13 @@ def assignRole(userID, role):
     user.role = role
     return None
     #function is subject to change with User Scripts
-    
+
+def createEvent(eventName, eventDate, eventTime, eventLocation):
+    newEvent = Event(eventName=eventName, 
+                    eventDate=eventDate,
+                    time=eventTime,
+                    location=eventLocation)
+    db.session.add(newEvent)
+    db.session.commit()
+    return newEvent
+        
