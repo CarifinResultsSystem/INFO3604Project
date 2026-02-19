d16718b056c12a4cf3ad655031c9a20ab5320a74
Jameel Ali <J1m2e3A4i@gmail.com>
2026-02-19 07:08:49 -0400

    Create event.py

---
+++ backend/App/models/event.py
@@ -0,0 +1,14 @@
+from backend.App.database import db
+
+class Event(db.Model):
+    eventID = db.Column(db.Integer, primary_key=True)
+    eventName = db.Column(db.String(256), nullable=False)
+    eventDate = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    location = db.Column(db.String(256), nullable=False)
+    
+    def __init__(self, eventName, eventDate, time, location):
+        self.eventName = eventName
+        self.eventDate = eventDate
+        self.time = time
+        self.location = location
