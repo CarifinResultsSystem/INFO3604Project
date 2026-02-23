a3fbe9f80c67b81fbd979f7af83465736ec42b16
Jameel Ali <J1m2e3A4i@gmail.com>
2026-02-23 15:52:07 -0400

    Created Instution Scripts
    Created Models and controllers for Institution

---
+++ App/models/institution.py
@@ -0,0 +1,17 @@
+from App.database import db
+
+class Institution(db.Model):
+    
+    institutionID = db.Column(db.Integer, primary_key=True)
+    insName = db.Column(db.String(256), nullable=False)
+    insTotalPoints = db.Column(db.Integer)
+    
+    def __init__(self, insName):
+        self.insName = insName
+        self.insTotalPoints = 0
+        
+    def get_json(self):
+        return{
+            'insName': self.insName,
+            'insTotalPoints': self.insTotalPoints
+        }
