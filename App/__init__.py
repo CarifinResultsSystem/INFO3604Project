50b9623fbcd658306a61b6b1f1beb004f3d911f2
Nathan-Baptiste <157825144+Nathan-Baptiste@users.noreply.github.com>
2026-02-24 20:32:25 -0400

    Create User CLI Test

--- App/__init__.py
+++ App/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask.cli import with_appcontext
+import click
 
 from .views import views, setup_admin
 from .config import load_config
 from .database import *
 from .controllers import setup_jwt
+from .controllers.user import *
 
 db = SQLAlchemy()
 
@@ -21,9 +24,39 @@ def create_app(overrides={}):
     init_db(app)
     jwt = setup_jwt(app)
     setup_admin(app)
+
     @jwt.invalid_token_loader
     @jwt.unauthorized_loader
     def custom_unauthorized_responce(error):
         return #render_template('401.html', error=error), 401 <= To be modified when templates are made
+    
+#------------------------ USER CLI TESTS -------------------------
+    
+    # Create User (flask create-user <username> <role> <email> <password>)
+    @app.cli.command("create-user")
+    @click.argument("username")
+    @click.argument("role")
+    @click.argument("email")
+    @click.argument("password")
+    @with_appcontext
+    def create_user_command(username, role, email, password):
+        try:
+            user = create_user(username, role, email, password)
+            click.echo(f"User created successfully: {user}")
+        except ValueError as e:
+            click.echo(f"Error: {e}")
+
+
+#------------------------ ADMIN CLI TESTS ------------------------
+#------------------------ JUDGE CLI TESTS ------------------------
+#---------------------- SCORETAKER CLI TESTS ---------------------
+#------------------------ EVENT CLI TESTS ------------------------
+#------------------------ SEASON CLI TESTS -----------------------
+#--------------------- INSTITUTION CLI TESTS ---------------------
+#--------------------- PARTICIPANT CLI TESTS ---------------------
+#--------------------- LEADERBOARD CLI TESTS ---------------------
+#--------------------- POINTS RULES CLI TESTS --------------------
+#------------------ AUTOMATED RESULTS CLI TESTS ------------------
+    
     app.app_context().push()
     return app
--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,4 +1,4 @@
-from sqlite3 import IntegrityError
+from sqlalchemy.exc import IntegrityError
 from App.models import User
 from App.database import db
 
