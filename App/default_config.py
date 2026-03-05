e081d96852440cf73ec7bf80205fbc766b95da2a
Nathan-Baptiste <157825144+Nathan-Baptiste@users.noreply.github.com>
2026-03-04 21:48:24 -0400

    config changes

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,4 @@
-SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
-SECRET_KEY="secret key"
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///temp-database.db')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret key')
