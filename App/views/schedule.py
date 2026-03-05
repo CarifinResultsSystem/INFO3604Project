9aeda26d4ba6ef27669392bf2f290ed234aa3cdf
Nathan-Baptiste <157825144+Nathan-Baptiste@users.noreply.github.com>
2026-03-04 22:30:36 -0400

    Add season filtering to participants & schedule

--- App/views/schedule.py
+++ App/views/schedule.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, jsonify, request, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
+from datetime import date
+from App.database import db
+from App.models import Event, Season
+
 schedule_views = Blueprint('schedule_views', __name__, template_folder='../templates')
 
 
@@ -10,7 +14,13 @@ Page/Action Routes
 
 @schedule_views.route('/schedule', methods=['GET'])
 def get_schedule_page():
-    return render_template('user/schedule.html')
+    events  = db.session.execute(db.select(Event).order_by(Event.eventDate.asc())).scalars().all()
+    seasons = db.session.execute(db.select(Season).order_by(Season.year.desc())).scalars().all()
+    return render_template('user/schedule.html',
+        events=events,
+        seasons=seasons,
+        today=date.today()
+    )
 
 
 '''
