731a9d95bfbabe77c08970f720fa9d454c9888cf
Jameel Ali <J1m2e3A4i@gmail.com>
2026-03-08 02:11:09 -0400

    Update Initialization for testing

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,5 +1,12 @@
+from uuid import uuid4
+
 from App.database import db
 from .user import create_user
+from .participant import create_participant
+from .institution import create_institution
+from .pointsRules import create_individual_rule, create_team_rule
+from .event import create_event
+from .season import create_season
 
 def initialize():
     db.drop_all()
@@ -8,3 +15,13 @@ def initialize():
     create_user('alice', 'judge', 'alice@mail.com', 'alicepass')
     create_user('john', 'scoretaker', 'john@mail.com', 'johnpass')
     create_user('eve', 'hr', 'eve@mail.com', 'evepass')
+    season = create_season(2026)
+    institution = create_institution("Scotiabank", "Port of Spain")
+    event = create_event("1 Lap Savannah", "2026-06-15", "09:00","Queens Park Savannah", season.seasonID)
+    create_participant(str(uuid4()), "Dave", "Darren", "Male", "2000-01-01", "San Fernando", institution.institutionID, [event[0].eventID])
+    create_individual_rule(event[0].eventID, season.seasonID, 1, "Gold", 15)
+    create_individual_rule(event[0].eventID, season.seasonID, 2, "Silver", 10)
+    create_individual_rule(event[0].eventID, season.seasonID, 3, "Silver", 5)
+    rule1 = create_team_rule(event[0].eventID, season.seasonID, "Participation/Attendance", "Mr. Carifin Representative", 10)
+    rule2 = create_team_rule(event[0].eventID, season.seasonID, "Participation/Attendance", "Mrs. Carifin Representative", 10)
+    rule3 = create_team_rule(event[0].eventID, season.seasonID, "Participation/Attendance", "Team Uniformity", 10)
