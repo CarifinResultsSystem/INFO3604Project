1741f9d3ee83e366085a6883e9ce1b50cec80ae9
Jameel Ali <J1m2e3A4i@gmail.com>
2026-04-03 00:10:57 -0400

    Update default_config.py
    Render Database should work

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,9 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///temp-database.db')
+SQLALCHEMY_DATABASE_uri = os.environ.get('DATABASE_URL', 'sqlite:///temp-database.db')
+
+if SQLALCHEMY_DATABASE_uri.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_uri = SQLALCHEMY_DATABASE_uri.replace('postgres://', 'postgresql://', 1)
+
+SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_uri
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret key')
